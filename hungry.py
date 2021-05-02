969c2beedf48cc3188064bd680073b5d895af23f
Narasimha Sai <anarasimhasai24@gmail.com>
2021-05-02 16:27:53 +0530

    added new line

--- hungry.py
+++ hungry.py
@@ -1,7 +1,9 @@
 inp = input("Are you hungry ? ")
+
+# this is the line we are adding
 if(inp.lower() == "yes" or inp.lower() == "y"):
+	print("Eat Pizza")
 	print("Eat Burger")
 else:
 	print("Drint Soft Drinks")
 
-[print(i) for i in range(len(inp))] 
